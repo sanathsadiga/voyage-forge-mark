9efbfa7423130aa1ef1b8021336193d106ed9f1d
sanathsadiga <sanathadigas@gmail.com>
2025-10-13 17:37:22 +0530

    fixed some GSC issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,34 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      // Redirect www to non-www
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.voyage-forge.com',
-          },
-        ],
-        destination: 'https://voyage-forge.com/:path*',
-        permanent: true,
-      },
-      // Redirect http to https
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'header',
-            key: 'x-forwarded-proto',
-            value: 'http',
-          },
-        ],
-        destination: 'https://voyage-forge.com/:path*',
-        permanent: true,
-      },
-    ]
-  },
   images: {
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
